c0e3443f3102cd44c9d6f92581c05fc8f758cf41
crdx <pub@crdx.org>
2023-01-19 17:47:24 +0000

    Add godoc comments

--- main.go
+++ main.go
@@ -6,15 +6,30 @@ import (
 
 var enable = true
 
-func Enable()  { enable = true }
-func Disable() { enable = false }
+// Enable enables colours.
+func Enable() {
+	enable = true
+}
 
+// Disable disables colours.
+func Disable() {
+	enable = false
+}
+
+// Init initialises colours based on heuristics.
+//
+// The following heuristics are used to figure out whether colour support should be disabled.
+//
+//   - The NO_COLOR environment variable is set.
+//   - The terminal is not interactive.
 func Init() {
 	InitUnless(false)
 }
 
-// Initialise colours.
-// Use the `disable` parameter to override autodetection.
+// InitUnless initialises colours based on heuristics unless overridden.
+//
+// Like [Init], but optionally override the heuristics and force colours to be disabled if disable
+// is set to true.
 func InitUnless(disable bool) {
 	if disable {
 		Disable()
@@ -31,6 +46,65 @@ func InitUnless(disable bool) {
 		return
 	}
 }
+
+// Green sets the foreground colour to green.
+func Green(str string) string { return render(str, fgGreen, false) }
+
+// Blue sets the foreground colour to blue.
+func Blue(str string) string { return render(str, fgBlue, false) }
+
+// Red sets the foreground colour to red.
+func Red(str string) string { return render(str, fgRed, false) }
+
+// Yellow sets the foreground colour to yellow.
+func Yellow(str string) string { return render(str, fgYellow, false) }
+
+// Magenta sets the foreground colour to magenta.
+func Magenta(str string) string { return render(str, fgMagenta, false) }
+
+// White sets the foreground colour to white.
+func White(str string) string { return render(str, fgWhite, false) }
+
+// Black sets the foreground colour to black.
+func Black(str string) string { return render(str, fgBlack, false) }
+
+// Cyan sets the foreground colour to cyan.
+func Cyan(str string) string { return render(str, fgCyan, false) }
+
+// BgGreen sets the background colour to green.
+func BgGreen(str string) string { return render(str, bgGreen, true) }
+
+// BgBlue sets the background colour to blue.
+func BgBlue(str string) string { return render(str, bgBlue, true) }
+
+// BgRed sets the background colour to red.
+func BgRed(str string) string { return render(str, bgRed, true) }
+
+// BgYellow sets the background colour to yellow.
+func BgYellow(str string) string { return render(str, bgYellow, true) }
+
+// BgMagenta sets the background colour to magenta.
+func BgMagenta(str string) string { return render(str, bgMagenta, true) }
+
+// BgWhite sets the background colour to white.
+func BgWhite(str string) string { return render(str, bgWhite, true) }
+
+// BgBlack sets the background colour to black.
+func BgBlack(str string) string { return render(str, bgBlack, true) }
+
+// BgCyan sets the background colour to cyan.
+func BgCyan(str string) string { return render(str, bgCyan, true) }
+
+// Underline sets the styling to underlined.
+func Underline(str string) string { return render(str, underline, false) }
+
+// Bold sets the styling to bold.
+func Bold(str string) string { return render(str, bold, false) }
+
+// Italic sets the styling to italic.
+func Italic(str string) string { return render(str, italic, false) }
+
+// —————————————————————————————————————————————————————————————————————————————————————————————————
 
 func isInteractive() bool {
 	fileInfo, _ := os.Stdout.Stat()
@@ -75,25 +149,3 @@ func render(str string, code string, bg bool) string {
 
 	return escape + code + "m" + str + escape + reset + "m"
 }
-
-func Green(str string) string   { return render(str, fgGreen, false) }
-func Blue(str string) string    { return render(str, fgBlue, false) }
-func Red(str string) string     { return render(str, fgRed, false) }
-func Yellow(str string) string  { return render(str, fgYellow, false) }
-func Magenta(str string) string { return render(str, fgMagenta, false) }
-func White(str string) string   { return render(str, fgWhite, false) }
-func Black(str string) string   { return render(str, fgBlack, false) }
-func Cyan(str string) string    { return render(str, fgCyan, false) }
-
-func BgGreen(str string) string   { return render(str, bgGreen, true) }
-func BgBlue(str string) string    { return render(str, bgBlue, true) }
-func BgRed(str string) string     { return render(str, bgRed, true) }
-func BgYellow(str string) string  { return render(str, bgYellow, true) }
-func BgMagenta(str string) string { return render(str, bgMagenta, true) }
-func BgWhite(str string) string   { return render(str, bgWhite, true) }
-func BgBlack(str string) string   { return render(str, bgBlack, true) }
-func BgCyan(str string) string    { return render(str, bgCyan, true) }
-
-func Underline(str string) string { return render(str, underline, false) }
-func Bold(str string) string      { return render(str, bold, false) }
-func Italic(str string) string    { return render(str, italic, false) }
